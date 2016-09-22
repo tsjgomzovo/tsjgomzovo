160a85d27111e8f5ac92a94af49589beb1fd4f51
Aleksandr Elmekeev <donamok@gmail.com>
2016-09-22 17:53:23 +0300

    trying to fix date i18n for github pages

--- _plugins/i18n_filter.rb
+++ _plugins/i18n_filter.rb
@@ -1,32 +1,24 @@
 require 'i18n'
 
-LOCALE = 'ru'
+LOCALE = :ru
 
 module Jekyll
   module I18nFilter
+
+    # Example:
+    #   {{ post.date | localize: "%d.%m.%Y" }}
+    #   {{ post.date | localize: ":short" }}
     def localize(input, format=nil)
       load_translations
       format = (format =~ /^:(\w+)/) ? $1.to_sym : format
-      I18n.l input, :format => format, :locale => LOCALE
+      I18n.l input, :format => format
     end
 
     def load_translations
-      unless I18n::backend.instance_variable_get(:@translations) and locale_file_exists
+      unless I18n::backend.instance_variable_get(:@translations)
         I18n.backend.load_translations Dir[File.join(File.dirname(__FILE__),'../_locales/*.yml')]
+        I18n.locale = LOCALE
       end
-    end
-
-    def locale_file_exists
-      file_name = File.join(LOCALE,'.yml')
-      return (locale_file_exists_in_current_dir(file_name) and locale_exists_in_locales_dir(file_name))
-    end
-
-    def locale_file_exists_in_current_dir(file_name)
-        return File.exists?(File.join(File.dirname(__FILE__),file_name))
-    end
-
-    def locale_exists_in_locales_dir(file_name)
-        return File.exists?(File.join('../_locales/',file_name))
     end
   end
 end
